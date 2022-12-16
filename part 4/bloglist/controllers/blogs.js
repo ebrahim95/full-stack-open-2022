f2fa8dad189120efdaec241b39773d9ef22e5382
ebrahim95 <ebrahim.akh95@gmail.com>
2022-12-16 14:42:45 -0600

    completed till 4.19

--- part 4/bloglist/controllers/blogs.js
+++ part 4/bloglist/controllers/blogs.js
@@ -1,28 +1,61 @@
 const blogsRouter = require('express').Router()
+const JsonWebToken  = require('jsonwebtoken')
 const Blog = require('../models/blog')
+const User = require('../models/user')
+
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.status(201).json(blogs)
 })
 
 blogsRouter.post('/', async (request, response, next) => {
-  const blogBody = request.body
+  let { title, author, url, likes } = request.body
+  const token = getTokenFrom(request)
+  // eslint-disable-next-line no-undef
+  const decodedToken = JsonWebToken.verify(token, process.env.SECRET)
 
-  if (!blogBody.likes) {
-    blogBody.likes = 0
+  if (!decodedToken.id) {
+    response.status(401).json({
+      error: 'token missing or invalid'
+    })
   }
 
-  if (!blogBody.url || !blogBody.title) {
+  const user = await User.findById(decodedToken.id)
+
+  if (!likes) {
+    likes = 0
+  }
+
+  if (!url || !title) {
     response.status(400).end()
   }
 
+  console.log(user)
 
-  const blog = new Blog(request.body)
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+    user: user._id
+  })
 
   try {
+
     const savedBlog = await blog.save()
+    user.blogs = [...user.blogs, savedBlog._id]
+    await user.save()
     response.status(201).json(savedBlog)
+
   } catch (expection) {
     next(expection)
   }
--- part 4/bloglist/models/blog.js
+++ part 4/bloglist/models/blog.js
@@ -4,10 +4,13 @@ const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
-  likes: Number
+  likes: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 blogSchema.set('toJSON', {
   transform: (document, returnedObject) => {
@@ -16,5 +19,7 @@ blogSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+const Blog = mongoose.model('Blog', blogSchema)
 
 module.exports = Blog
