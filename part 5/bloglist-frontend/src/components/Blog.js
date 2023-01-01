c7bbaedceffa3039135307b25a54c1fb37590edb
ebrahim95 <ebrahim.akh95@gmail.com>
2023-01-01 01:35:52 -0600

    Lint edits

--- part 5/bloglist-frontend/src/components/Blog.js
+++ part 5/bloglist-frontend/src/components/Blog.js
@@ -1,23 +1,23 @@
-import { useState } from "react"
+import { useState } from 'react'
 
 const Blog = ({ blog, handleLikes, handleDelete, user }) => {
   const blogStyle = {
-    paddingTop: 10, 
+    paddingTop: 10,
     paddingBottom: 10,
     paddingLeft: 2,
     border: 'solid',
-    borderWidth: 2, 
+    borderWidth: 2,
     marginBottom: 5
   }
   const [visible, setVisible] = useState(false)
   const updateBlog = {
-      title: blog.title,
-      author: blog.author,
-      url: blog.url,
-      likes: blog.likes + 1,
-      user: blog.user.id,
-      id: blog.id
-    }
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes + 1,
+    user: blog.user.id,
+    id: blog.id
+  }
   const updateLikeCount = () => {
     handleLikes(blog.id, updateBlog)
   }
@@ -34,17 +34,17 @@ const Blog = ({ blog, handleLikes, handleDelete, user }) => {
         {blog.url}<br/>
         likes: {blog.likes} {'   '} <button onClick={updateLikeCount}>like</button><br/>
         {blog.user.name}
-        <br/>{ blog.user.username === user.username ? <button onClick={deleteBlog}>remove</button> : ""}
+        <br/>{ blog.user.username === user.username ? <button onClick={deleteBlog}>remove</button> : ''}
       </div>
     )
   }
 
   return (
-  <div style={blogStyle}>
-    {blog.title} {blog.author} {'   '}
-    <button onClick={handleView}>{ visible ? 'Hide' : 'View'} </button>
-    {visible && viewDetails()}
-  </div>  
-)}
+    <div style={blogStyle}>
+      {blog.title} {blog.author} {'   '}
+      <button onClick={handleView}>{ visible ? 'Hide' : 'View'} </button>
+      {visible && viewDetails()}
+    </div>
+  )}
 
 export default Blog
