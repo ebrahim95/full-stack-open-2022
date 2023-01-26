d32cd5b3a3fe39a10f9611e2e9761439bc0cadb5
ebrahim95 <ebrahim.akh95@gmail.com>
2023-01-25 23:17:23 -0600

    finished till 6.14 - fixed bugs

--- part 6/redux-anecdotes/src/reducers/filterReducer.js
+++ part 6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = "dd"
+const initialState = ""
 
 const filterSlice = createSlice({
     name: 'filter',
--- part 6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part 6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = "hello"
+const initialState = null
 const notificationSlice = createSlice({
     name: 'notifications',
     initialState, 
