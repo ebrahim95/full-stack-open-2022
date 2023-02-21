251b9eac9bb1d561d3a2b8b3727cc79e96b2034b
ebrahim95 <ebrahim.akh95@gmail.com>
2023-02-20 23:42:56 -0600

    fixed  bugs

--- part 7/bloglist-frontend-redux/frontend/src/components/BlogForm.js
+++ part 7/bloglist-frontend-redux/frontend/src/components/BlogForm.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { changeNotification } from "../reducers/notificationReducer";
 import { addBlog } from "../reducers/blogReducer";
 import { useDispatch } from "react-redux";
--- part 7/bloglist-frontend-redux/frontend/src/services/blogs.js
+++ part 7/bloglist-frontend-redux/frontend/src/services/blogs.js
@@ -39,4 +39,5 @@ const remove = async (id) => {
   return response.data;
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, setToken, create, update, remove, addComment };
