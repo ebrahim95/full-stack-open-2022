57203fe31333a29c74a9d58519798b1afedb44eb
ebrahim95 <ebrahim.akh95@gmail.com>
2023-03-04 19:25:21 -0600

    finished till 9.5

--- part 9/bmiCalculator.ts
+++ part 9/bmiCalculator.ts
@@ -3,7 +3,6 @@ const weight: number = Number(process.argv[3]);
 
 const calculateBmi = (height: number, weight: number): string => {
   const BMI: number = (weight / (height / 100) ** 2);
-  console.log(BMI)
   if (BMI < 25) {
     return 'Normal (Healthy Weight)'
   } else if (BMI >= 30) {
@@ -11,8 +10,8 @@ const calculateBmi = (height: number, weight: number): string => {
   } else if (BMI >= 25 || BMI <= 29) {
     return 'Overweight (Unhealthy Weight)'
   }
-
+  return 'something went wrong'
 }
 
 console.log(calculateBmi(height, weight))
-
+export default calculateBmi 
