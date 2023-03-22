95b7b2b3824e2080a287dc8a3385834909c14dd4
ebrahim95 <ebrahim.akh95@gmail.com>
2023-03-22 01:15:24 -0500

    finished till 9.19

--- part 9/flight-diary/frontend/src/App.tsx
+++ part 9/flight-diary/frontend/src/App.tsx
@@ -1,19 +1,90 @@
-import { useEffect } from 'react';
-import './App.css';
+import { ChangeEvent, SyntheticEvent, useEffect } from 'react';
 import { useState } from 'react';
-import { DiaryEntry } from './types';
-import { getDiaries } from './services/diaryService';
+import { DiaryEntry, NewDiaryEntry, Visibility, Weather } from './types';
+import { getDiaries, addDiaryEntry } from './services/diaryService';
+import axios from 'axios';
 const App = () => {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [addEntry, setEntry] = useState<NewDiaryEntry>({
+    date: "",
+    visibility: Visibility.Ok,
+    weather: Weather.Sunny,
+    comment: ""
+  });
+  const [notification, setNotification] = useState<string>("");
 
   useEffect(() => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     getDiaries().then(data => setDiaries(data));
   }, []);
+
+  const addDiary = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    try {
+      await addDiaryEntry(addEntry);
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.data && typeof error.response?.data === 'string') {
+          setNotification(error.response?.data);
+        }
+      } else {
+        console.error(error);
+      }
+
+    }
+  };
+
+  const handleAddEntry = (event: ChangeEvent<HTMLInputElement>) => {
+    const value = event.target.value;
+    setEntry({
+      ...addEntry,
+      [event.target.name]: value
+    });
+  };
+  const handleAddSelectEntry = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setEntry({
+      ...addEntry,
+      [event.target.name]: value
+    });
+  };
+
+
   return (
     <div>
-      {diaries.map(diary => <div key={diary.id}>{diary.date}</div>)}
-    </div>
+      <h2>Add Diary Entry</h2>
+      <div>
+        {notification === "" ? "" : <p>{notification}</p>}
+      </div>
+      <form onSubmit={void addDiary}>
+        Date <input type='date' name='date' onChange={handleAddEntry} /><br />
+        Visibility<select name='visibility' onChange={handleAddSelectEntry}>
+          <option value={Visibility.Great}> Great </option>
+          <option value={Visibility.Good}> Good </option>
+          <option value={Visibility.Ok}> Ok </option>
+          <option value={Visibility.Poor}> Poor </option>
+        </select><br />
+        Weather<select name='weather' onChange={handleAddSelectEntry}>
+          <option value={Weather.Sunny}> Sunny </option>
+          <option value={Weather.Rainy}> Rainy </option>
+          <option value={Weather.Windy}> Windy </option>
+          <option value={Weather.Stormy}> Stormy </option>
+          <option value={Weather.Cloudy}> Cloudy </option>
+        </select><br />
+        Comment<input name='comment' onChange={handleAddEntry} /><br />
+        <button type='submit'>Add</button>
+      </form>
+      <h2>Diary Entries</h2>
+      {
+        diaries.map(diary => <div key={diary.id}>
+          {diary.date}
+          <p>
+            Visibility: {diary.visibility}<br />
+            Weather: {diary.weather}
+          </p>
+        </div>)
+      }
+    </div >
   );
 };
 export default App;
--- part 9/flight-diary/frontend/src/services/diaryService.ts
+++ part 9/flight-diary/frontend/src/services/diaryService.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NewDiaryEntry } from "../types";
 
 const baseUrl = "http://localhost:3001/api/diaries";
 
 export const getDiaries = async () => {
   const response = await axios.get<DiaryEntry[]>(baseUrl);
+  return response.data;
+};
+
+export const addDiaryEntry = async (diary_entry: NewDiaryEntry) => {
+  const response = await axios.post<NewDiaryEntry>(baseUrl, diary_entry);
   return response.data;
 };
