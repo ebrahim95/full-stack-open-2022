f5e589323f4aaaa45197d3d796e7e7577b35cf61
ebrahim95 <ebrahim.akh95@gmail.com>
2023-03-25 15:57:08 -0500

    refactored the code

--- part 9/flight-diary/frontend/src/App.tsx
+++ part 9/flight-diary/frontend/src/App.tsx
@@ -14,14 +14,14 @@ const App = () => {
   const [notification, setNotification] = useState<string>("");
 
   useEffect(() => {
-    // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    getDiaries().then(data => setDiaries(data));
+    void getDiaries().then(data => setDiaries(data));
   }, []);
 
   const addDiary = async (event: SyntheticEvent) => {
     event.preventDefault();
     try {
-      await addDiaryEntry(addEntry);
+      const response = await addDiaryEntry(addEntry);
+      setDiaries([...diaries, response as DiaryEntry]);
     } catch (error) {
       if (axios.isAxiosError(error)) {
         if (error.response?.data && typeof error.response?.data === 'string') {
@@ -54,16 +54,30 @@ const App = () => {
     <div>
       <h2>Add Diary Entry</h2>
       <div>
-        {notification === "" ? "" : <p>{notification}</p>}
+        {notification === "" ? "" : <p style={{ color: 'red' }}>{notification}</p>}
       </div>
-      <form onSubmit={void addDiary}>
+      <form onSubmit={addDiary}>
         Date <input type='date' name='date' onChange={handleAddEntry} /><br />
-        Visibility<select name='visibility' onChange={handleAddSelectEntry}>
-          <option value={Visibility.Great}> Great </option>
-          <option value={Visibility.Good}> Good </option>
-          <option value={Visibility.Ok}> Ok </option>
-          <option value={Visibility.Poor}> Poor </option>
-        </select><br />
+        <fieldset>
+          <legend> Choose Visibility </legend>
+          <input onChange={handleAddEntry} type="radio" name='visibility' id={Visibility.Great} value={Visibility.Great} />
+          <label htmlFor={Visibility.Great}>{Visibility.Great}</label><br />
+
+          <input onChange={handleAddEntry} type="radio" name='visibility' id={Visibility.Good} value={Visibility.Good} />
+          <label htmlFor={Visibility.Good}>{Visibility.Good}</label><br />
+
+          <input onChange={handleAddEntry} type="radio" name='visibility' id={Visibility.Ok} value={Visibility.Ok} />
+          <label htmlFor={Visibility.Ok}>{Visibility.Ok}</label><br />
+
+          <input onChange={handleAddEntry} type="radio" name='visibility' id={Visibility.Poor} value={Visibility.Poor} />
+          <label htmlFor={Visibility.Poor}>{Visibility.Poor}</label><br />
+        </fieldset>
+        {/* Visibility<select name='visibility' onChange={handleAddSelectEntry}> */}
+        {/*   <option value={Visibility.Great}> Great </option> */}
+        {/*   <option value={Visibility.Good}> Good </option> */}
+        {/*   <option value={Visibility.Ok}> Ok </option> */}
+        {/*   <option value={Visibility.Poor}> Poor </option> */}
+        {/* </select><br /> */}
         Weather<select name='weather' onChange={handleAddSelectEntry}>
           <option value={Weather.Sunny}> Sunny </option>
           <option value={Weather.Rainy}> Rainy </option>
